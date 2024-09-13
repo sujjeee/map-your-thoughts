541a49ce4b764df19f242d5d01f2b3ce5ddc9a83
Suraj <sujjeee@outlook.com>
2024-09-13 13:10:48 +0530

    update: test geo

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,15 +1,13 @@
 import { geoLocation } from "@/lib/geo"
 
 export default function GeoPage() {
-  const { country, ip1, country1, city, ip } = geoLocation()
+  const { country, city, ip } = geoLocation()
 
   return (
     <div>
       <h1>Geolocation Information</h1>
-      <p>Country1: {country1}</p>
       <p>Country: {country}</p>
       <p>City: {city}</p>
-      <p>IP Address1: {ip1}</p>
       <p>IP Address: {ip}</p>
     </div>
   )
--- src/lib/geo.tsx
+++ src/lib/geo.tsx
@@ -2,11 +2,10 @@ import { headers } from "next/headers"
 
 export function geoLocation() {
   const headersList = headers()
+  
   return {
-    country1: headersList.get("x-vercel-ip-country-region") || "Unknown",
-    country: headersList.get("x-vercel-ip-country") || "Unknown",
-    city: headersList.get("x-vercel-ip-city") || "Unknown",
-    ip1: headersList.get("x-real-ip") || "Unknown",
-    ip: headersList.get("x-forwarded-for")?.split(",")[0] || "Unknown",
+    country: headersList.get("x-vercel-ip-country"),
+    city: headersList.get("x-vercel-ip-city"),
+    ip: headersList.get("x-real-ip"),
   }
 }
